6fb6f918c8e4dba679da81f949a9c406ce306da5
smurfedGitHubIO <clydefrongoso@gmail.com>
2022-11-28 15:03:55 +0800

    Abakoda A and B

---
+++ ABaKoDa 2022 Round 3/B Beautiful Kyut Delightful Sequence.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long int ll;
+
+int main(){
+  ll n;
+  cin >> n;
+  if(n == 1){
+    cout << 1;
+  } else {
+    cout << 3;
+  }
+  return 0;
+}
