1d10354e51a6977578ad4f390a9bb4768958bae2
Justin Clyde V. Frongoso <clydefrongoso@gmail.com>
2023-08-01 09:58:11 +0800

    feat: add 289b soln

--- 201-300/289/289B.cpp
+++ 201-300/289/289B.cpp
@@ -6,32 +6,25 @@ typedef long long int ll;
 int main(){
     ll n, m, k;
     cin >> n >> m >> k;
-    ll A[n][m];
-    map<ll, ll> mp;
-    ll sm = 0;
-    for(ll i=0; i<n; i++){
-        for(ll j=0; j<m; j++){
-            cin >> A[i][j];
-            mp[A[i][j]] = 1;
-            sm += A[i][j];
-        }
+    ll A[n*m];
+    ll mn = 10000000000, sm = 0;
+    for(ll i=0; i<n*m; i++){
+        cin >> A[i];
+        sm += A[i];
     }
-    vector<ll> v;
-    for(auto x : mp){
-        v.emplace_back(x.first);
-    }
-    if(v.size() == 1){
+    sort(A, A+(n*m));
+    if(n*m == 1){
         cout << 0;
     }else{
-        sort(begin(v), end(v));
-        ll mn = 1000000000000;
-        for(ll i=0; i<v.size(); i++){
-            if(i < v.size()-1 and abs(v[i]-v[i+1])%k != 0){
+        ll cur = 0;
+        for(ll i=0; i<n*m; i++){
+            if(abs(A[i]-A[i+1])%k != 0 and i<n*m-1){
                 cout << -1;
                 return 0;
             }
-            cout << sm << " " << v[i]*n*m << "\n";
-            mn = min(mn, abs(sm-n*m*v[i])/k);
+            sm -= A[i];
+            mn = min(mn, ((sm-A[i]*(n*m-(i+1))+(A[i]*i-cur))/k));
+            cur += A[i];
         }
         cout << mn;
     }
