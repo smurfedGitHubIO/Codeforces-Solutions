ab433fe513b3a6fbd2c99be4bf232e7cb5c21646
Justin Clyde V. Frongoso <clydefrongoso@gmail.com>
2023-08-03 21:48:57 +0800

    feat: add 137b soln and template

---
+++ template.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+#define f first
+#define s second
+#define mp make_pair
+using namespace std;
+
+typedef long long int ll;
+typedef pair<ll, ll> pll;
+
+ll xs[] = {-1, 1, 0, 0, -1, -1, 1, 1};
+ll ys[] = {0, 0, -1, 1, -1, 1, 1, -1};
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    return 0;
+}
