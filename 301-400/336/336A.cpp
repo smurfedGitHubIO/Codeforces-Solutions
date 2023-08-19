2778219eb9c8b2bb29721ce96deb41b0b70148df
Justin Clyde V. Frongoso <clydefrongoso@gmail.com>
2023-08-19 22:51:31 +0800

    feat: add 336a soln

--- 301-400/336/336A.cpp
+++ 301-400/336/336A.cpp
@@ -6,10 +6,14 @@ typedef long long int ll;
 int main(){
     ll x, y;
     cin >> x >> y;
-    if((y+x)*(x<0) < 0){
-        cout << y+x << " 0 0 " << x-y;
-    }else {
-        cout << "0 " << x-y << " " << y-x << " 0";
+    if(x > 0 and y > 0){
+        cout << "0 " << x+y << " " << x+y << " 0";
+    }else if (x < 0 and y > 0){
+        cout << -(abs(x)+y) << " 0 0 " << abs(x)+y;
+    }else if (x > 0 and y < 0){
+        cout << "0 " << -(x+abs(y)) << " " << x+abs(y) << " 0";
+    }else{
+        cout << x+y << " 0 0 " << x+y;
     }
     return 0;
 }
